7b52d99005cdf2c54f10cfeaa096c1e4f2797842
Amirdone <amiralibagheriiii90@gmail.com>
2024-11-04 13:48:54 +0330

    test comp

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,23 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { Calendar } from "@/components/ui/calendar"
+import { Breadcrumb } from "@/components/ui/breadcrumb";
+
+
+import { Button } from "@/components/ui/button";
 import Navbar from "@/components/navbar";
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <Button variant="link">Button</Button>
+      <Calendar/>
+      <Breadcrumb/>
     </div>
   );
 }
