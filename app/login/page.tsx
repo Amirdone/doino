1ea75acd0e4012e3dfef0e33592cf300116f93d1
Amirdone <amiralibagheriiii90@gmail.com>
2024-11-13 23:19:21 +0330

    test 4

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Card,
   CardContent,
@@ -6,22 +8,109 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-const login = () => {
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "لطفا این قسمت را خالی نگذارید.",
+  }),
+});
+
+export default function ProfileForm() {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
   return (
-   <div className= "flex flex-row min-h-screen justify-center items-center">
-    <Card>
-      <CardHeader>
-        <CardTitle>ورود|عضویت</CardTitle>
-        <CardDescription>شماره یا ایمیل خود را وارد کنید</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <p>ورود</p>
-      </CardContent>
-      <CardFooter>
-        <p>ورود شما به معنای پذیرش قوانین است</p>
-      </CardFooter>
-    </Card>
-   </div>
+    <div className="flex  min-h-screen justify-center items-center ">
+      <Card className="p-5 m-5">
+        <CardHeader>
+          <CardTitle> ورود | عضویت</CardTitle>
+        </CardHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormDescription>
+                    لطفا شماره موبایل یا ایمیل خود را وارد کنید
+                  </FormDescription>
+                  <FormControl>
+                    <Input placeholder="" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button className="w-full m-0 bg-green-500 " variant="outline" type="submit">
+            ورود
+            </Button>
+            <p>
+              ورود شما به معنای پذیرش
+              <Button className="p-1 " variant="link">
+                قوانین
+              </Button>
+              است
+            </p>
+          </form>
+        </Form>
+      </Card>
+    </div>
   );
-};
-export default login;
+}
+//const login = () => {
+//return (
+//<div className="flex flex-row min-h-screen justify-center items-center">
+//<Card>
+//<CardHeader>
+//<CardTitle> ورود | عضویت</CardTitle>
+//</CardHeader>
+//<CardContent>
+//<CardDescription className="m-4 w-full">
+//  شماره یا ایمیل خود را وارد کنید
+//</CardDescription>
+//<Input className="w-full " type="text" placeholder="" />
+//<Button className="w-full m-0 " variant="outline">
+//  ورود
+//</Button>
+//</CardContent>
+//<CardFooter className="text-caption text-neutral-700 mt-4">
+//<p>
+//   ورود شما به معنای پذیرش
+// <Button className="p-1" variant="link">
+// قوانین
+//</Button>
+// است
+//</p>
+//</CardFooter>
+//</Card>
+//</div>
+//);
+//};
+//export default onsubmit;
