c0e00730df73aa1b0e4a89acb4284c62d622c452
Amirdone <amiralibagheriiii90@gmail.com>
2025-01-24 16:15:32 +0330

    landing fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Footer from "@/components/footer";
+import { redirect } from 'next/navigation';
 import Image from 'next/image'
 import doino from "@/public/doino.svg"
 import { Button } from "@/components/ui/button";
@@ -8,17 +9,23 @@ import Navbar from "@/components/navbar";
 import BottomNav from "@/components/BottomNav";
 import LandingPage from "./landing/page";
 export default function Home() {
-  return (
-    <div className=" bg-slate-100 ">
-      <div>
-        <LandingPage/>
-        <Navbar />
-        <BottomNav/>
-      </div>
-      <div className="pt-24">
-        <LandingSectionKanban />
-        <Footer/>
-      </div>
-    </div>
-  );
+  redirect('/landing');
+  return null; // این خط ضروری نیست ولی می‌تونه نوشته بشه
 }
+//export default function Home() {
+  //return (
+    //<div className=" bg-slate-100 ">
+      //<div>
+      //  <LandingPage/>
+      //  <Navbar />
+      //  <BottomNav/>
+      //</div>
+     // <div className="pt-24">
+        //<LandingSectionKanban />
+       // <Footer/>
+      //</div>
+    //</div>
+   
+    
+  //);
+//}
